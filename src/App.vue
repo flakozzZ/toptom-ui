918a151202db77ed2954c87bf00886985dcff38c
Abay <a.kosaev@inbox.ru>
2024-02-29 19:22:18 +0600

    changes

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { useI18n } from 'vue-i18n'
 import StorybookHeader from '@/header/index.vue'
 import ProgressBar from '@/components/progress-bar/index.vue'
+import TooltipComponent from '@/components/tooltip/index.vue'
 
 const i18n = useI18n()
 
@@ -10,6 +11,8 @@ console.log(i18n.t('test-message'))
 i18n.locale.value = 'en'
 
 console.log(i18n.t('test-message'))
+
+const text = "Text"
 </script>
 
 <template>
@@ -17,6 +20,12 @@ console.log(i18n.t('test-message'))
     <div class='container'>
       <storybook-header/>
       <progress-bar/>
+      <div class='tooltip'>
+        <tooltip-component :text='text'/>
+        <tooltip-component :text='text'/>
+        <tooltip-component :text='text'/>
+        <tooltip-component :text='text'/>
+      </div>
     </div>
   </main>
 </template>
@@ -27,6 +36,11 @@ console.log(i18n.t('test-message'))
   width: 100%;
   max-width: 1280px;
   margin: 0 auto;
+}
+
+.tooltip {
+  display: flex;
+  gap: 12px;
 }
 
 
